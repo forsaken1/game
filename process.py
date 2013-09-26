1e79b35c12c5cd4da91b27b720747be312bb0fb6
Alexey Krylov <alexey2142@gmail.com>
2013-09-27 04:38:02 +1100

    Сделал signup

--- process.py
+++ process.py
@@ -1,46 +1,76 @@
-from flask import jsonify
+from flask import jsonify, session
+import MySQLdb, re
 
-def process(req):
-	params = req['params']
-	proc = {"signup":		signup(params), 
-			"signin":		signin(params), 
-			"signout":		signout(params),
-			"sendMessage":	sendMessage(params),
-			"getMessages":	getMessages(params),
-			"createGame":	createGame(params),
-			"leaveGame":	leaveGame(params),
-			"getGames":		getGames(params),
-			"joinGame":		joinGame(params),
-			"loadMap":		loadMap(params),
-			}
-	return proc.get(req['action'])
+class Process:
+	def __init__(self):
+		self.db = MySQLdb.connect(host='localhost', port=3306, user='root', passwd='', db='game')
+		
+	def __del__(self):
+		self.db.close()
+	
+	def process(self, req):
+		params = req['params']
+		proc = 	{
+			'signup':		self.signup(params),
+			'signin':		self.signin(params),
+			'signout':		self.signout(params),
+			'sendMessage':	self.sendMessage(params),
+			'getMessages':	self.getMessages(params),
+			'createGame':	self.createGame(params),
+			'leaveGame':	self.leaveGame(params),
+			'getGames':		self.getGames(params),
+			'joinGame':		self.joinGame(params),
+			'loadMap':		self.loadMap(params),
+		}
+		return proc.get(req['action'])
 
-def signup(par):
-	return par['password']
-	
-def signin(par):
-	return par['login']
-	
-def signout(par):
-	return par['login']
-	
-def sendMessage(par):
-	return par['login']
-	
-def getMessages(par):
-	return par['login']
-	
-def createGame(par):
-	return par['login']
+	def signup(self, par):
+		reg_exp_login = re.compile('\w{4,40}', re.IGNORECASE)
+		result = reg_exp_login.match(par['login'])
+		if not result:
+			return jsonify(result='badLogin', message='Bad login')
+		
+		reg_exp_pass = re.compile('\w{4,}', re.IGNORECASE)
+		result = reg_exp_pass.match(par['password'])
+		if not result:
+			return jsonify(result='badPassword', message='Bad password')
+		
+		cur = self.db.cursor()
+		cur.execute('SELECT `login` FROM `user` WHERE `login` = %s', (par['login'],))
+		login = cur.fetchall()
+		if login:
+			return jsonify(result='userExists', message='User exists')
+		
+		cur.execute('INSERT INTO `user` (`login`, `password`) VALUES (%s, %s)', (par['login'], par['password'],))
+		self.db.commit()
+		return jsonify(result='ok', message='Successful signup')
+		
+	def signin(self, par):
+		return par['login']
+		
+	def signout(self, par):
+		return par['login']
+		
+	def sendMessage(self, par):
+		return par['login']
+		
+	def getMessages(self, par):
+		return par['login']
+		
+	def createGame(self, par):
+		return par['login']
 
-def leaveGame(par):
-	return par['login']
-	
-def getGames(par):
-	return par['login']
-	
-def joinGame(par):
-	return par['login']
-	
-def loadMap(par):
-	return par['login']
+	def leaveGame(self, par):
+		return par['login']
+		
+	def getGames(self, par):
+		return par['login']
+		
+	def joinGame(self, par):
+		return par['login']
+		
+	def loadMap(self, par):
+		return par['login']
+
+	def unknownAction(self):
+		return jsonify(result=unknownAction, message='Unknown action')
--- run.py
+++ run.py
@@ -1,16 +1,26 @@
-from flask import Flask, request, jsonify, render_template
-import json
-import process
+from flask import Flask, request, render_template
+from process import Process
+import re
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def index():
-	return process.process(request.json)
+	p = Process()
+	return p.process(request.json)
 
 @app.route('/test', methods = ['GET'])
 def test():
     return render_template('test.html')
 
-if __name__ == "__main__":
+@app.route('/foo', methods = ['GET'])
+def foo():
+	reg_exp = re.compile('\w{4,40}', re.IGNORECASE)
+	answer = reg_exp.match('qwe1')
+	if answer:
+		return '123'
+	else:
+		return '321'
+
+if __name__ == '__main__':
     app.run(debug = True)
