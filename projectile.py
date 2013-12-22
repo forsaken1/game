7cdb45b438b5e2e9af3c6be688ded8d1ecdc3f8b
pinkslot <pinkslot@mail.ru>
2013-12-22 17:41:13 +1100

    init projectile

---
+++ projectile.py
@@ -0,0 +1,11 @@
+#from point import *
+
+weapons = {
+		   'P': (1, 10, 10, .3),
+		   'M': (1, 10, 5, .3),
+		   'K': (.5, 5, 3, .5),
+		   'R': (1, 30, 25, 0.5),
+		   'A': (100, 15, 15, 0.3)}
+
+#class projectile(object):
+#	def __init__(self, type, k, dir):
